d3090b8f7f03b99fde1e3f4fc5af4277315fb168
Khalid Hoffman <khaliddpdev@gmail.com>
2016-01-26 12:07:06 -0500

    added more documentation for spa builder. some refactoring.

--- spa-config-builder/index.js
+++ spa-config-builder/index.js
@@ -15,30 +15,38 @@ function isPHP(fileName) {
 }
 
 module.exports = {
+    /**
+     *
+     * @param {Object} options
+     * @param {String} [options.projectName]
+     * @param {String} [options.srcDirectory]
+     * @param {String} [options.themeDirectory]
+     * @param {String} [options.filename='views.json']
+     * @param {Object} [options.context]
+     */
     build: function (options) {
-        var defaults = {
+        var _options = _.extend({
                 srcDirectory: path.resolve('src/', 'pages'),
                 themeDirectory: path.resolve('wp-content/themes/', (options && options.projectName) ? options.projectName : 'dp-boilerplate/'),
                 writeDirectory: path.resolve('src/', 'modules/'),
                 filename: 'views.json'
-            },
+            }, options),
             scriptsList = [],
             pagesList = [],
             definedPages = [],
             isPageListComplete = false,
-            isScriptListComplete = false,
-            settings = _.assign({}, defaults, options);
+            isScriptListComplete = false;
 
         function onFilesRead() {
             //console.log('Finding intersection between:', scriptsList, pagesList);
             console.log('Finding intersection...');
             definedPages = _.intersection(scriptsList, pagesList);
-            var jsonPath = path.resolve(settings.writeDirectory, settings.filename),
-                rjsFixPath = path.resolve(settings.writeDirectory, 'pages-stub.js'),
+            var jsonPath = path.resolve(_options.writeDirectory, _options.filename),
+                rjsFixPath = path.resolve(_options.writeDirectory, 'pages-stub.js'),
                 productionPages = _.without(definedPages, 'page-debug');
 
-            productionPages = productionPages.map(function(pageName){
-                return 'pages/'+pageName
+            productionPages = productionPages.map(function (pageName) {
+                return 'pages/' + pageName
             });
             var rjsFix = 'define(' + JSON.stringify(productionPages) + ', function(){});';
             fs.writeFile(jsonPath, JSON.stringify(productionPages), function () {
@@ -49,26 +57,28 @@ module.exports = {
             });
         }
 
-        Walker.recursePath(settings.themeDirectory, {
+        Walker.recursePath(_options.themeDirectory, {
             each: function (err, filepath) {
                 if (err) throw err;
                 var filename = path.basename(filepath);
                 if (isPHP(filename)) pagesList.push(path.basename(filepath, '.php'));
             },
-            done: function (err, filelist) {
+            done: function (err, fileList) {
                 if (err) throw err;
                 isPageListComplete = true;
-                if (isScriptListComplete) onFilesRead.apply();
+                if (isScriptListComplete) onFilesRead.apply(_options.context, fileList);
             }
         });
-        Walker.recursePath(settings.srcDirectory, {
+        Walker.recursePath(_options.srcDirectory, {
             each: function (err, filepath) {
+                if (err) throw err;
                 var filename = path.basename(filepath);
                 if (isScript(filename)) scriptsList.push(path.basename(filepath, '.js'));
             },
-            done: function (err, filelist) {
+            done: function (err, fileList) {
+                if (err) throw err;
                 isScriptListComplete = true;
-                if (isPageListComplete) onFilesRead.apply();
+                if (isPageListComplete) onFilesRead.apply(_options.context, fileList);
             }
         });
     }
