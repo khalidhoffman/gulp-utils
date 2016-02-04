b33a3a03b9f1bf4ab8c2c28526c5a602af217b72
Khalid Hoffman <khaliddpdev@gmail.com>
2016-02-03 19:09:52 -0500

    includes fixes for celebrationtt theme

--- fs-walk/index.js
+++ fs-walk/index.js
@@ -4,19 +4,23 @@ var _ = require('lodash'),
 /**
  *
  * @param rootDir
- * @param options {object} fields: each, done
+ * @param options {object}
+ * @param options.each {Function}
+ * @param options.done {Function}
+ * @param options.context {Object}
  */
 function recursePath(rootDir, options) {
     var results = [],
         remainingPathsCount = 0,
-        args = _.extend({}, options);
+        _options = _.extend({
+            context : null
+        }, options);
 
     function readPath(fsPath, settings) {
         fs.stat(fsPath, function (err, pathMeta) {
             if (err) {
                 console.error(err);
-                if (_.isFunction(settings.done)) settings.done.call(null, err);
-                if (_.isFunction(settings.each)) settings.each.call(null, err);
+                if (_.isFunction(settings.done)) settings.done.call(settings.context, err);
             } else if (pathMeta.isDirectory()) {
                 fs.readdir(fsPath, function (err, fileList) {
                     if (err) return done(err);
@@ -31,15 +35,15 @@ function recursePath(rootDir, options) {
                 results.push(fsPath);
                 if (_.isFunction(settings.each)) settings.each.call(null, null, fsPath);
                 remainingPathsCount--;
-                if (remainingPathsCount < 0 && _.isFunction(settings.done)) settings.done.call(null, null, results);
+                if (remainingPathsCount < 0 && _.isFunction(settings.done)) settings.done.call(settings.context, null, results);
             }
         });
     }
 
-    readPath(rootDir, args);
-};
+    readPath(rootDir, _options);
+}
 
 module.exports = {
     recursePath : recursePath,
     walk : recursePath
-}
+};
